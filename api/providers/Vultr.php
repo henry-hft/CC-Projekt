a890ed6880e2c7bf76b970cea5f37a1188cced73
henry-hft <333333henry@gmail.com>
2021-06-29 16:47:44 +0200

    Add plan endpoint

--- api/providers/Vultr.php
+++ api/providers/Vultr.php
@@ -40,6 +40,47 @@ class Vultr extends Provider {
 		}
 		return $response;
 	}
+		public function plans($id = null) {
+		$request = new Request();
+		$apikey = $this->token;
+		$header = "Accept-language: en\r\n" .
+				  "Authorization: Bearer $apikey\r\n" . 
+			     "Content-type: application/json\r\n";
+		$request->httpRequest("GET", "https://api.vultr.com/v2/plans", $header, "");
+		$response = $request->getResponse();
+		$decoded = json_decode($response);
+		$response = array('error' => false);
+		$planArray = array('plans' => array());
+		foreach($decoded->plans as $plans){
+			if (is_null($id)) {
+				$planArray['plans'][] = array(
+					"id" => $plans->id,
+					"cores" => $plans->vcpu_count,
+					"memory" => $plans->ram,
+					"disk" => $plans->disk * 1000,
+					"bandwidth" => $plans->bandwidth * 1000
+				);
+			} else {
+				if($plans->id == $id){
+					$response += array("plans" => array(
+									   "id" => $plans->id,
+									   "cores" => $plans->vcpu_count,
+									   "memory" => $plans->ram,
+					                   "disk" => $plans->disk * 1000,
+					                   "bandwidth" => $plans->bandwidth * 1000
+										)
+								);
+								break;
+				}
+			}
+		}
+		if (is_null($id)) {
+			$response += $planArray;
+		} else if (count($response) < 2) {
+			$response = array("error" => true, "message" => "Unknown server plan");
+		}
+		return $response;
+	}
   public function create(){
 	  
   }
