9ae49be702d9050dea8704ef2058412045f00a37
henry-hft <333333henry@gmail.com>
2021-06-29 16:47:17 +0200

    Add plan endpoint

--- api/providers/Hetzner.php
+++ api/providers/Hetzner.php
@@ -40,6 +40,47 @@ class Hetzner extends Provider {
 		}
 		return $response;
 	}
+		public function plans($id = null) {
+		$request = new Request();
+		$apikey = $this->token;
+		$header = "Accept-language: en\r\n" .
+				  "Authorization: Bearer $apikey\r\n" . 
+			     "Content-type: application/json\r\n";
+		$request->httpRequest("GET", "https://api.hetzner.cloud/v1/server_types", $header, "");
+		$response = $request->getResponse();
+		$decoded = json_decode($response);
+		$response = array('error' => false);
+		$planArray = array('plans' => array());
+		foreach($decoded->server_types as $plans){
+			if (is_null($id)) {
+				$planArray['plans'][] = array(
+					"id" => $plans->name,
+					"cores" => $plans->cores,
+					"memory" => $plans->memory * 1024,
+					"disk" => $plans->disk * 1000,
+					"bandwidth" => 20 * 1000 * 1024
+				);
+			} else {
+				if($plans->name == $id){
+					$response += array("plans" => array(
+											"id" => $plans->name,
+											"cores" => $plans->cores,
+											"memory" => $plans->memory * 1024,
+											"disk" => $plans->disk * 1000,
+											"bandwidth" => 20 * 1000 * 1024
+										)
+								);
+								break;
+				}
+			}
+		}
+		if (is_null($id)) {
+			$response += $planArray;
+		} else if (count($response) < 2) {
+			$response = array("error" => true, "message" => "Unknown server plan");
+		}
+		return $response;
+	}
   public function create(){
 	  
   }
