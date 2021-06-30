0109bbcd6be06c5b87099bc78be1c9c8fd9326a1
henry-hft <333333henry@gmail.com>
2021-06-30 21:53:36 +0200

    Add parameters

--- api/objects/Provider.php
+++ api/objects/Provider.php
@@ -6,8 +6,10 @@ abstract class Provider {
     $this->token = $token;
   }
   abstract public function locations($id = null);
+  abstract public function plans($id = null);
+  abstract public function os($id = null);
   abstract public function create();
-  abstract public function delete();
+  abstract public function delete($id);
   abstract public function status();
   abstract public function info();
   abstract public function start();
