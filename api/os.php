65e945547774446df92c7af4e34f7e29f47a6814
henry-hft <333333henry@gmail.com>
2021-06-30 21:02:34 +0200

    Create os.php

---
+++ api/os.php
@@ -0,0 +1,142 @@
+<?php
+include_once './config/core.php';
+include_once './config/database.php';
+include_once './objects/Request.php';
+ 
+// JWT library
+include_once './libs/php-jwt-master/src/BeforeValidException.php';
+include_once './libs/php-jwt-master/src/ExpiredException.php';
+include_once './libs/php-jwt-master/src/SignatureInvalidException.php';
+include_once './libs/php-jwt-master/src/JWT.php';
+use \Firebase\JWT\JWT;
+
+// required headers
+header("Access-Control-Allow-Origin: * ");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: GET");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+// Autoload all providers
+spl_autoload_register(function ($class_name) {
+    include 'providers/' . $class_name . '.php';
+});
+
+
+$data = json_decode(file_get_contents("php://input"));
+if(!empty($_SERVER['HTTP_AUTHORIZATION'])){
+	$arr = explode(" ", $_SERVER['HTTP_AUTHORIZATION']);
+	$jwt = $arr[1];
+	if (!empty($jwt)) {
+		try {
+			// instantiate database and server object
+			$database = new Database();
+			$db = $database->getConnection();
+	
+			$decoded = JWT::decode($jwt, $secret_key, array('HS256'));
+	
+			$query = "SELECT id, enabled FROM users WHERE id=:userid";
+			$stmt = $db->prepare($query);
+			$userid = $decoded->data->id;
+			$stmt->bindParam(":userid", $userid);
+			$stmt->execute();
+	
+			if ($stmt->rowCount() == 1) { // check if user exists
+				$stmt->bindColumn('id', $userid);
+				$stmt->bindColumn('enabled', $enabled);
+				$stmt->fetch(); 
+					if ($enabled == "1") { // check if the account of owner of the API key is enabled
+						if ($_SERVER['REQUEST_METHOD'] === 'GET') { // get server(s)
+							if (isset($_GET['provider'])) { // only one provider
+								$query = "SELECT name FROM providers WHERE name LIKE :providerName";
+								$stmt = $db->prepare($query);
+								$stmt->bindParam(":providerName", $_GET['provider']);
+								$stmt->execute();
+								
+								if ($stmt->rowCount() == 1) { // check if provider is valid
+									$providerName = $stmt->fetchColumn();
+									// get provider token (api key)
+									$header = "Authorization: Bearer $jwt\r\n" . 
+											  "Content-type: application/json\r\n";
+									$request = new Request();
+									
+									$request->httpRequest("GET", "http://193.196.52.234/cloud/CC-Projekt/api/token.php?provider=$providerName", $header, "");
+									$httpResponse = $request->getResponse();
+									$decoded = json_decode($httpResponse);
+									if($decoded->error === false){
+										$enabled = $decoded->token->enabled;
+										if($enabled === true){
+											$token = $decoded->token->token;
+											$providerObj = new $providerName($token);
+												if(!empty($_GET['id'])){
+													$osId = $_GET['id'];
+												} else {
+													$osId = null;
+												}
+												
+												if(!empty($_GET['family'])){
+													$osFamily = $_GET['family'];
+												} else {
+													$osFamily = null;
+												}
+
+												$locationResponse = $providerObj->os($osId, $osFamily);
+
+										
+											$response = $locationResponse;
+											if($locationResponse['error'] == "false"){
+												http_response_code(200);
+											} else {
+												http_response_code(400);
+											}
+												
+											
+											
+										}else{
+											$response = array ('error' => true, 'message' => 'Provider is disabled');
+											http_response_code(400);
+											exit(json_encode($response));
+										}
+									} else {
+										http_response_code(400);
+										exit($httpResponse);
+									}
+									
+									
+									
+								} else {
+									$response = array ('error' => true, 'message' => 'Invalid/unknown provider');
+									http_response_code(400);
+									exit(json_encode($response));
+								}		
+							} else {
+								$response = array ('error' => true, 'message' => 'Missing provider parameter');
+								http_response_code(400);
+							}		
+							
+						} else {
+							$response = array ('error' => true, 'message' => 'Invalid request method');
+							http_response_code(400);
+						}
+					} else { // Invalid/unknown API Key
+						$response = array ('error' => true, 'message' => 'Authentification failed');
+						http_response_code(400);
+					}
+			}else{
+				$response = array ('error' => true, 'message' => 'Invalid/unknown user');
+				http_response_code(400);
+			}
+		
+} catch (Exception $e) {
+			$response = array ('error' => true, 'message' => 'Authentication failed');
+			http_response_code(401);
+		}
+	} else {
+		$response = array ('error' => true, 'message' => 'Missing access token');
+	}	
+} else {
+	$response = array ('error' => true, 'message' => 'Missing access token');
+}
+echo json_encode($response);
+
+?>
