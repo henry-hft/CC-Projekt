e6f40dd27bd4edcffb968663c7411c2e711d6504
henry-hft <333333henry@gmail.com>
2021-06-30 21:36:15 +0200

    Add getStatusCode function for http requests

--- api/objects/Request.php
+++ api/objects/Request.php
@@ -2,6 +2,7 @@
 class Request {
 
 	private $response;
+	private $header;
 	
 	function httpRequest($method, $url, $header = "", $content = "") {
 		$opts = array(
@@ -14,10 +15,21 @@ class Request {
 
 		$context = stream_context_create($opts);
 		$this->response = file_get_contents($url, false, $context);
+		$this->header = $http_response_header;
 	}
 	
 	function getResponse(){
 		return $this->response;
+	}
+	
+	function getStatusCode(){
+		if (is_array($this->header)) {
+			$parts = explode(' ', $this->header[0]);
+			if (count($parts) > 1) { //HTTP/1.0 <code> <text>
+				return intval($parts[1]); //Get code
+			}
+		}
+		return 0;
 	}
 }
 ?>
