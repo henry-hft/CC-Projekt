95cee6def2113ea72d4a160416456dcaff8f2bcd
henry-hft <333333henry@gmail.com>
2021-07-02 12:08:45 +0200

    Change $provider to $allProviders

--- api/objects/Provider.php
+++ api/objects/Provider.php
@@ -5,9 +5,9 @@ abstract class Provider {
   public function __construct($token) {
     $this->token = $token;
   }
-  abstract public function locations($id = null, $provider = false);
-  abstract public function plans($id = null, $provider = false);
-  abstract public function os($id = null, $provider = false);
+  abstract public function locations($id = null, $allProviders = false);
+  abstract public function plans($id = null, $allProviders = false);
+  abstract public function os($id = null, $allProviders = false);
   abstract public function create($hostname, $location, $plan, $os, $sshkey, $script);
   abstract public function delete($id);
   abstract public function status();
