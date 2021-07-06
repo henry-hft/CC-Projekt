42ea59172395f89b6c0ba6999797d45b8cb54a40
henry-hft <333333henry@gmail.com>
2021-07-06 20:25:24 +0200

    Create login.php

---
+++ api/login.php
@@ -0,0 +1,77 @@
+<?php
+include_once './config/core.php';
+include_once './config/database.php';
+
+// JWT library
+include_once './libs/php-jwt-master/src/BeforeValidException.php';
+include_once './libs/php-jwt-master/src/ExpiredException.php';
+include_once './libs/php-jwt-master/src/SignatureInvalidException.php';
+include_once './libs/php-jwt-master/src/JWT.php';
+use \Firebase\JWT\JWT;
+
+// required headers
+header("Access-Control-Allow-Origin: * ");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+	$database = new Database();
+	$db = $database->getConnection();
+
+	if($_SERVER['REQUEST_METHOD'] === 'POST'){
+			if((!empty($_GET['username']) OR !empty($_GET['email'])) AND !empty($_GET['password'])){
+				if (!empty($_GET['username'])) { // log in with username
+					$query = "SELECT * FROM users WHERE username=:username";
+					$stmt = $db->prepare($query);
+					$stmt->bindParam(":username", $_GET['username']);
+				} else { // log in with email address
+					$query = "SELECT * FROM users WHERE email=:email";
+					$stmt = $db->prepare($query);
+					$stmt->bindParam(":email", $_GET['email']);
+				}
+				
+				$stmt->execute();
+				if ($stmt->rowCount() == 1) {
+					$row = $stmt->fetch(PDO::FETCH_ASSOC);
+					if (password_verify($_GET['password'], $row['password'])) {
+						
+						$issuer_claim = "CLOUDCOMPUTING";
+						$audience_claim = "USER";
+						$issuedat_claim = time(); // issued at
+						$notbefore_claim = $issuedat_claim + 0; //not before in seconds
+						$expire_claim = $issuedat_claim + 3600; // expire time in seconds
+						
+						$token = array(
+							"iss" => $issuer_claim,
+							"aud" => $audience_claim,
+							"iat" => $issuedat_claim,
+							"nbf" => $notbefore_claim,
+							"exp" => $expire_claim,
+							"data" => array(
+								"id" => $row['id']
+						));
+
+						$jwt = JWT::encode($token, $secret_key);
+						$response = array("error" => false, "message" => "Successful login", "jwt" => $jwt, "expireAt" => $expire_claim);
+						http_response_code(200);
+						
+					} else {
+						$response = array("error" => true, "message" => "Invalid password");
+						http_response_code(400);
+					}
+				} else {
+					$response = array("error" => true, "message" => "User not found");
+					http_response_code(400);
+				}
+				
+			} else {
+				$response = array("error" => true, "message" => "Missing parameter");
+				http_response_code(400);
+			}
+	} else {
+		$response = array("error" => true, "message" => "Invalid request method");
+		http_response_code(400);
+	}
+echo json_encode($response);
+?>
