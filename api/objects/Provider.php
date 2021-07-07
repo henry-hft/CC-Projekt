b2b3e60b187cbe49e50e9e5dd5aadfc41e03eeb6
henry-hft <333333henry@gmail.com>
2021-07-07 11:29:05 +0200

    Rename functions

--- api/objects/Provider.php
+++ api/objects/Provider.php
@@ -8,14 +8,14 @@ abstract class Provider {
   abstract public function locations($id = null, $allProviders = false);
   abstract public function plans($id = null, $allProviders = false);
   abstract public function os($id = null, $allProviders = false);
-  abstract public function create($hostname, $location, $plan, $os, $sshkey, $script);
+  abstract public function create($hostname, $location, $plan, $os, $sshkey, $script = null);
   abstract public function delete($id);
-  abstract public function status();
-  abstract public function info();
-  abstract public function start();
-  abstract public function stop($id);
+  abstract public function servers($id = null, $allProviders = false);
+  abstract public function control($id, $action);
   abstract public function createSSHKey($key);
   abstract public function deleteSSHKey($id);
+  abstract public function createScript($script);
+  abstract public function deleteScript($id);
 }
 
 ?>
