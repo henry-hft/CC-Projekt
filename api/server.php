5215864d408645da930f0c5b4846df377a66db57
henry-hft <333333henry@gmail.com>
2021-07-07 11:17:25 +0200

    Create server.php

--- api/server.php
+++ api/server.php
@@ -1,63 +1,159 @@
 <?php
+include_once './config/core.php';
+include_once './config/database.php';
+include_once './objects/Request.php';
+ 
+// JWT library
+include_once './libs/php-jwt-master/src/BeforeValidException.php';
+include_once './libs/php-jwt-master/src/ExpiredException.php';
+include_once './libs/php-jwt-master/src/SignatureInvalidException.php';
+include_once './libs/php-jwt-master/src/JWT.php';
+use \Firebase\JWT\JWT;
+
 // required headers
-header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin: * ");
 header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: GET");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
-include_once 'config/core.php';
-include_once 'config/database.php';
-
-session_start();
-
-if (isset($_GET['apikey'])) {
-	if (isset($_GET['provider'])) {
-		if (in_array($_GET['provider'], $providers)) { // check if provider is valid
+// Autoload all providers
+spl_autoload_register(function ($class_name) {
+    include 'providers/' . $class_name . '.php';
+});
+if($_SERVER['REQUEST_METHOD'] === 'GET'){
+if((!empty($_GET['id']) AND !empty($_GET['provider'])) OR (empty($_GET['id']) AND !empty($_GET['provider'])) OR (empty($_GET['id']) AND empty($_GET['provider']))){
+	if(!empty($_SERVER['HTTP_AUTHORIZATION'])){
+	$arr = explode(" ", $_SERVER['HTTP_AUTHORIZATION']);
+	$jwt = $arr[1];
+	if (!empty($jwt)) {
+		try {
 			// instantiate database and server object
 			$database = new Database();
 			$db = $database->getConnection();
 	
-			$query = "SELECT userid, enabled FROM users WHERE apikey=?";
-			$stmt = $this->conn->prepare($query);
+			$decoded = JWT::decode($jwt, $secret_key, array('HS256'));
 	
-			$stmt->bind_param("s", $_GET['apikey']);
+			$query = "SELECT id, enabled FROM users WHERE id=:userid";
+			$stmt = $db->prepare($query);
+			$userid = $decoded->data->id;
+			$stmt->bindParam(":userid", $userid);
 			$stmt->execute();
-			$stmt->store_result();
 	
-			if ($stmt->num_rows == 1) { // check if API key exists
-				$stmt->bind_result($userid, $enabled);
-				$stmt->fetch();
-				if ($enabled == "1") { 
+			if ($stmt->rowCount() == 1) { // check if user exists
+				$stmt->bindColumn('id', $userid);
+				$stmt->bindColumn('enabled', $enabled);
+				$stmt->fetch(); 
 					if ($enabled == "1") { // check if the account of owner of the API key is enabled
 						if ($_SERVER['REQUEST_METHOD'] === 'GET') { // get server(s)
-							if (isset($_GET['id']) { // get only one server
-					
-							} else { // get all servers
-					
-							}
-						} else if($_SERVER['REQUEST_METHOD'] === 'POST') { // create server
-						
-						} else if($_SERVER['REQUEST_METHOD'] === 'DELETE') { // delete server
-						
-						} else {
-							$response = array ('success' => false, 'message' => 'Invalid request method');
-						}
-					} else { // Invalid/unknown API Key
-						$response = array ('success' => false, 'message' => 'Authentification failed');
-					}
-				}else{
-				
-				}
-			}else{
-				$response = array ('success' => false, 'message' => 'Invalid/unknown API key');
-			}
-		} else {
-			$response = array ('success' => false, 'message' => 'Invalid/unknown provider');
-		}		
-	} else {
-		$response = array ('success' => false, 'message' => 'Missing provider parameter');
-	}		
-} else {
-	// TODO: check session
-}
-echo json_encode($response);
+							if (!empty($_GET['provider'])) { // only one provider
+								$query = "SELECT name FROM providers WHERE name LIKE :providerName";
+								$stmt = $db->prepare($query);
+								$stmt->bindParam(":providerName", $_GET['provider']);
+								$stmt->execute();
+								
+								if ($stmt->rowCount() == 1) { // check if provider is valid
+									$providerName = $stmt->fetchColumn();
+								} else {
+									$response = array ('error' => true, 'message' => 'Invalid/unknown provider');
+									http_response_code(400);
+									exit(json_encode($response));
+								}			
+							}		
+									
+									
+									$query = "SELECT * FROM tokens WHERE userid=:userid";
+									$stmt = $db->prepare($query);
+									$stmt->bindParam(":userid", $userid);
+									$stmt->execute();
+									$providers = array();
+								
 
+								$response = array();
+									while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+											$provider = $row['provider'];
+											$providerToken = $row['token'];
+											$enabled = $row['enabled'];
+											if(isset($providerName)){
+												if($provider == $providerName){
+													if($enabled == "1"){
+														$providerObj = new $provider($providerToken);
+														if(!empty($_GET['id'])){
+															$serversResponse = $providerObj->servers($_GET['id']);
+														} else {
+															$serversResponse = $providerObj->servers();
+														}		
+														$response = $serversResponse;
+													} else {
+														$response = array ('error' => true, 'message' => 'Provider is disabled');
+														http_response_code(400);
+														exit(json_encode($response));
+													}
+													break;
+												}
+											} else {
+												if($enabled == "1"){
+													$providerObj = new $provider($providerToken);
+													$serversResponse = $providerObj->servers(null, true);
+													
+													if($serversResponse['error'] == false){ 
+														if(count($response) >= 1){
+															$response = array_merge($response, $serversResponse["servers"]);
+														} else {
+															$response = array_merge($response, $serversResponse["servers"]);
+														}
+													} else {
+														$response = array ('error' => true, 'message' => 'Cloud not get requested data');
+														http_response_code(400);
+														exit(json_encode($response));
+													}
+												}
+											}
+									}
+									
+									if(!isset($providerName)){
+										$response = array("error" => false, "servers" => $response);
+									}
+									
+									if(count($response) > 1){
+											if($response['error'] == "false"){
+												http_response_code(200);
+											} else {
+												http_response_code(400);
+											}
+									} else {
+										$response = array ('error' => true, 'message' => 'No enabled providers found');
+										http_response_code(400);
+									}
+						} else {
+							$response = array ('error' => true, 'message' => 'Invalid request method');
+							http_response_code(400);
+						}
+					} else {
+						$response = array ('error' => true, 'message' => 'Authentification failed');
+						http_response_code(400);
+					}
+			}else{
+				$response = array ('error' => true, 'message' => 'Invalid/unknown user');
+				http_response_code(400);
+			}
+		
+} catch (Exception $e) {
+			$response = array ('error' => true, 'message' => 'Authentication failed');
+			http_response_code(401);
+		}
+	} else {
+		$response = array ('error' => true, 'message' => 'Missing access token');
+	}	
+} else {
+	$response = array ('error' => true, 'message' => 'Missing access token');
+}
+} else {
+	$response = array ('error' => true, 'message' => 'Invalid parameter');
+}
+} else {
+		$response = array("error" => true, "message" => "Invalid request method");
+		http_response_code(400);
+	}
+echo json_encode($response);
 ?>
