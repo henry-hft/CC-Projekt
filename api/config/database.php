5dd1b41898b1bde462de7fadc242829b0c8d2cfb
henry-hft <333333henry@gmail.com>
2021-07-15 09:47:51 +0200

    Update database.php

--- api/config/database.php
+++ api/config/database.php
@@ -2,24 +2,24 @@
 class Database
 {
     // specify database credentials
-    if (getenv("SQL_HOST") && getenv("SQL_DATABASE") && getenv("SQL_USER") && getenv("SQL_PASSWORD")) {
-        private $host = getenv("SQL_HOST");
-        private $db_name = getenv("SQL_DATABASE");
-        private $username = getenv("SQL_USER");
-        private $password = getenv("SQL_PASSWORD");
-    } else {
-        private $host = "localhost";
-        private $db_name = "cloud";
-        private $username = "user";
-        private $password = "test";
-    }
+    private $host = "localhost";
+    private $db_name = "cloud";
+    private $username = "user";
+    private $password = "test";
     public $conn;
 
     // get the database connection
     public function getConnection()
     {
         $this->conn = null;
-
+    
+        if (getenv("SQL_HOST") && getenv("SQL_DATABASE") && getenv("SQL_USER") && getenv("SQL_PASSWORD")) {
+             $host = getenv("SQL_HOST");
+             $db_name = getenv("SQL_DATABASE");
+             $username = getenv("SQL_USER");
+             $password = getenv("SQL_PASSWORD");
+        }
+        
         try {
             $this->conn = new PDO(
                 "mysql:host=" . $this->host . ";dbname=" . $this->db_name,
